3f6f957f3748b4c59b064c4904ab609b29d02773
JesusHades <jesusmanriquez56@aragon.unam.mx>
2022-11-22 18:54:11 -0600

    Se hicieron algunos cambios

--- src/Balanceo.java
+++ src/Balanceo.java
@@ -4,12 +4,12 @@ public class Balanceo {
     
 public static boolean Balanceo(String s) {
         Stack<Character> stack = new Stack<Character>();
-        // Iterate through string until empty
+       
         for(int i = 0; i<s.length(); i++) {
-            // Push any open parentheses onto stack
+            
             if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
                 stack.push(s.charAt(i));
-            // Check stack for corresponding closing parentheses, false if not valid
+           
             else if(s.charAt(i) == ')' && !stack.empty() && stack.peek() == '(')
                 stack.pop();
             else if(s.charAt(i) == ']' && !stack.empty() && stack.peek() == '[')
@@ -19,7 +19,7 @@ public static boolean Balanceo(String s) {
             else
                 return false;
         }
-        // return true if no open parentheses left in stack
+      
         return stack.isEmpty();
     }
 }
